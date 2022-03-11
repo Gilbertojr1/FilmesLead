6554e7e27535816e7faed702e2123f4cf60bd84c
Gilberto Costa Teixeira Junior <gilberto.junior@lead.fortaleza>
2022-03-11 14:44:24 -0300

    Spring security

--- src/main/java/com/br/lead/filmesLead/config/security/SecurityConfigurations.java
+++ src/main/java/com/br/lead/filmesLead/config/security/SecurityConfigurations.java
@@ -1,14 +1,20 @@
 package com.br.lead.filmesLead.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.br.lead.filmesLead.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -16,6 +22,18 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	
 	//Configuracoes de autenticacao
 	@Override
@@ -29,11 +47,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET ,"/api/").permitAll()
 		.antMatchers(HttpMethod.GET ,"/api/*").permitAll()
+		.antMatchers(HttpMethod.GET ,"/api/filmes/*").permitAll()
+		.antMatchers(HttpMethod.POST ,"/auth*").permitAll()
 		.anyRequest().authenticated()
-		.and().formLogin();
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
 	
-	//Configuracao de recuros estaticos
+	//Configuracao de recursos estaticos
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 	}
